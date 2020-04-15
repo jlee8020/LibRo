caf3586b9c444b02e80a3f3e1c3564036de2ec1c
Jan Lee <janlee@Jans-MacBook-Pro.local>
2020-04-15 17:24:19 -0500

    deployed

--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -6,8 +6,9 @@ const HomePage = (props) => {
     return (
         <main>
                 <h3 className={styles.h3}>Welcome!</h3>
-                <section className={styles.dSection}>
-                        <p>Test</p>
+                <section className={styles.dSection}>        
+                <p>Welcome to LibRo!</p>
+                <p>Sign in or Sign Up to start keeping track of your books.</p>
                 </section>    
         </main>
     );
